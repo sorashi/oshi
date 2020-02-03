940b6b3191548f6715acbe53eb634abc58aef8dc
Dennis Prazak <prazak.dennis@gmail.com>
2020-02-03 16:17:01 +0100

    Use a JSON file as database

--- oshi/__main__.py
+++ oshi/__main__.py
@@ -1,13 +1,6 @@
 """
 oshi -- Japanese grammar tester
 """
-import gzip
-import os
-from lxml import etree
+import database
 
-WORKING_DIRECTORY = os.path.dirname(__file__)
-tree = None
-with gzip.open(os.path.join(WORKING_DIRECTORY, 'JMdict_e.gz')) as f:
-    tree = etree.parse('JMdict_e.xml')
-# assert isinstance(tree, etree.ElementTree)
-print(tree.docinfo.internalDTD)
+db = database.connect()
---
+++ oshi/database.py
@@ -0,0 +1,62 @@
+"""
+Database abstraction
+"""
+import json
+import re
+import gzip
+from os import path
+from lxml import etree
+
+CURRENT_DIRECTORY = path.dirname(__file__)
+DATABASE_FILENAME = path.join(CURRENT_DIRECTORY, 'oshi_database.json')
+
+class Database:
+    """
+    Represents a database connection
+    """
+    def __init__(self, entries):
+        self.entries = entries
+
+def connect(filename=DATABASE_FILENAME):
+    """
+    Loads database and returns a Database object
+    """
+    if not path.exists(filename):
+        raise FileNotFoundError("Database file not found: " + filename)
+    with open(filename, 'r') as f:
+        return Database(json.load(f))
+
+def build(filename="JMdict_e.gz", output_filename=DATABASE_FILENAME):
+    """
+    Builds database from a JMdict gzip or xml file and writes to database
+    into output_filename
+    Should be used from a command-line
+    """
+    extension = path.splitext(filename)[1].lower()
+    if extension == ".gz":
+        with gzip.open(filename) as f:
+            tree = etree.parse(f)
+    elif extension == ".xml":
+        tree = etree.parse(filename)
+    else:
+        raise ValueError("File extension not supported: " + extension)
+
+    entries = []
+    # variables starting with x contain xml element(s)
+    for xentry in tree.getroot():
+        entry = {}
+        entry["writings"] = [x.find('keb').text for x in xentry.findall('k_ele')]
+        entry["readings"] = [x.find('reb').text for x in xentry.findall('r_ele')]
+        xsenses = xentry.findall('sense')
+        senses = []
+        for xsense in xsenses:
+            tags = []
+            for xtag in xsense.findall('pos') + xsense.findall('misc'):
+                for match in re.search(r'&(.*?);', xtag.text) or []:
+                    tags.append(match)
+            glosses = [x.text for x in xsense.findall('gloss')]
+            senses.append({"glosses": glosses, "tags": tags})
+        entry["senses"] = senses
+        entries.append(entry)
+    with open(output_filename, 'w') as f:
+        json.dump(entries, f)
