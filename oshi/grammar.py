b0be4f72d7da237de470b245e9b65df73dca7362
Dennis Prazak <prazak.dennis@gmail.com>
2020-02-04 19:00:40 +0100

    Parse grammar

---
+++ oshi/grammar.py
@@ -0,0 +1,90 @@
+import re
+from os import path
+from fnmatch import fnmatch # fnmatch(test_string, glob_pattern)
+from typing import List
+
+RULE_REGEX = r'^(\S+)\s*(\S*)\s+〜(\S*)\s*(\S*)\s+for\s+(\S*)\s+〜(\S*) +((?:[ \t]*\S+)+)\s*$'
+
+CURRENT_DIRECTORY = path.dirname(__file__)
+RULES_FILENAME = path.join(CURRENT_DIRECTORY, 'grammar.rules')
+
+VOWEL_KATAKANA = list("アイウエオ")
+SOUND_CHANGE = {
+    "": [""]*9,
+    "ア": list("さかがまばならわた"),
+    "イ": list("しきぎみびにりいち"),
+    "ウ": list("すくぐむぶぬるうつ"),
+    "エ": list("せけげめべねれえて"),
+    "オ": list("そこごもぼのろおと")
+}
+
+class Rule:
+    """
+    Represents a grammar rule
+    RULE      [ROLE] PATTERN [POS] for TARGET TARGET_PATTERN POS-GLOB...
+    potential plain  〜エる  v1    for plain  〜ウ           v5[^r]* v5r vs-c
+    """
+    def __init__(self, rule: str, role: str, pattern: str, pos: str, target: str,
+                 target_pattern: str, pos_globs: List[str]):
+        assert type(pos_globs) is list
+        self.rule = rule
+        self.role = role
+        self.pattern = pattern
+        self.pos = pos
+        self.traget = target
+        self.target_pattern = target_pattern
+        self.pos_globs = pos_globs
+    def __str__(self):
+        string = self.rule
+        if self.role:
+            string += " " + self.role
+        string += " ~" + self.pattern
+        if self.pos:
+            string += " " + self.pos
+        string += " for {} ~{} {}".format(self.traget, self.target_pattern, " ".join(self.pos_globs))
+        return string
+    def __repr__(self):
+        return "{} {} ~{} {} for {} ~{} {}".format(self.rule, self.role, self.pattern, self.pos, self.traget, self.target_pattern, " ".join(self.pos_globs))
+
+def parse_rules(filename=RULES_FILENAME):
+    rules = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        line_number = 0
+        for line in f:
+            line_number += 1
+            if re.match(r'^\s*#.*$', line): # skip comment lines
+                continue
+            if re.match(r'^\s*$', line):    # skip empty lines
+                continue
+            line = re.sub(r'#.*', "", line) # remove comments
+            match = re.match(RULE_REGEX, line)
+            if not match:
+                raise SyntaxError("Error parsing line {}: {}".format(line_number, line))
+            rule, role, pattern, pos, target, target_pattern, pos_globs = match.groups()
+            pos_globs = pos_globs.split()
+            # role and pos are optional
+            # pattern and target_pattern can be empty
+            assert "" not in [rule, target]
+            assert len(pos_globs) > 0
+            role = None if role == "" else role
+            pos = None if pos == "" else pos
+            if len(pattern) <= 0 or pattern[0] not in VOWEL_KATAKANA:
+                rules.append(Rule(rule, role, pattern, pos, target, target_pattern, pos_globs))
+            else:
+                # expansion required
+                expansion_regex = r'^([アイウエオ]?).*$'
+                sound = re.match(expansion_regex, pattern)[1]
+                assert sound != "" # is guaranteed by previous if statement
+                # sound_target can be empty
+                sound_target = re.match(expansion_regex, target_pattern)[1]
+                assert 9 == len(SOUND_CHANGE[sound]) == len(SOUND_CHANGE[sound_target])
+                for i in range(9):
+                    rules.append(Rule(rule, role,
+                                      re.sub(expansion_regex, SOUND_CHANGE[sound][i], pattern),
+                                      pos, target,
+                                      re.sub(expansion_regex, SOUND_CHANGE[sound_target][i], target_pattern),
+                                      pos_globs))
+        return rules
+if __name__ == "__main__":
+    for rule in parse_rules():
+        print(repr(rule))
